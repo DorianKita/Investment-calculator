bad01df75428cf892630b2bd60f59d419b882b81
Dorian Kita <dorian.kita90@gmail.com>
2024-02-02 14:02:06 +0100

    lifter userInput state from UserInout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleUserInput(userInputFieldValue, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        userInputFieldValue: newValue,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChangeInput={handleUserInput} userInput={userInput} />
+      <Results />
     </>
   );
 }
